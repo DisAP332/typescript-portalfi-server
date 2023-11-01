34e9f10fbe859a650e0f600b97525e2e5f364f7a
DAP <disap332@gmail.com>
2023-11-01 14:15:26 -0400

    test

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,11 +1,7 @@
 import express from "express";
-import cookieParser from "cookie-parser";
+import user_controller from "./userController";
 
 const router = express.Router();
-
-router.use(cookieParser());
-
-import user_controller from "./userController";
 
 router.post("/login", user_controller.loginUser);
 
